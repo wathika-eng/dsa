d55356edbca96165e0f396310c9d0e1d14339159
wathika-eng <wathika02@gmail.com>
2024-10-05 17:55:12 +0300

    go_implementation

---
+++ linear_search.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func linear_search(arr []int, target int) (int, error) {
+	for i := range arr {
+		if arr[i] == target {
+			return i, nil
+		}
+	}
+	return -1, errors.New("not found")
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	ret, err := linear_search(arr, 11)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	} else {
+		fmt.Printf("Value found at index %v\n", ret)
+	}
+}
